3eca0d49195bad3c31bc5d37fabd9af0747bcaa7
remarkstech <tech@remarks.asia>
2025-03-21 22:55:23 +0700

    Update app.py

--- app/app.py
+++ app/app.py
@@ -121,7 +121,6 @@ if st.button("Calculate Cost"):
             # Display results
             st.success(f"**Cost Estimation:** IDR {predicted_cost[0][0]:,.0f}")
             if margin != 0:  # Hanya tampilkan jika margin bukan 0
-                st.success(f"**Cost Estimation:** IDR {predicted_cost[0][0]:,.0f}")
                 st.success(f"**Cost Estimation with Margin:** IDR {predicted_cost2[0][0]:,.0f}")
         except Exception as e:
             st.error("An error occurred during prediction.")
